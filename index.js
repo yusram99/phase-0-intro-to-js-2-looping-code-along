cab2abc4dab17ac824a38d09dee68b22e9adc456
Your Name <youremail@example.com>
2023-06-18 17:06:34 +0300

    commit message

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+function writeCards(names) {
+const thankYouMessages = [];
+for (let i = 0; i < names.length; i++) {
+thankYouMessages.push(
+`Thank you, ${names[i]}, for the wonderful surprise gift!`
+);
+}
+return thankYouMessages;
+}
+const number = 10;
+function countDown(number) {
+while (number >= 0) {
+console.log(number);
+number--;
+}
+}
